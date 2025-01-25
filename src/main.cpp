fa73e1dd3a9802b95c6ae0b4b43d8eb9ac763170
kalpakavindu <kalpadevonline@gmail.com>
2025-01-25 21:00:54 +0530

    source: added source code from h5p9sl/hmac_sha256

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
+#include <cassert>
+#include <cstdint>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-#include "sha1.hpp"
+#include "hmac_sha256.h"
+
+#define SHA256_HASH_SIZE 32
 
 int main() {
-  std::string msg = "The quick brown fox jumps over the lazy dog";
+  const std::string secret = "key";
+  const std::string data = "The quick brown fox jumps over the lazy dog";
+  std::stringstream res;
 
-  SHA1 sha1;
-  sha1.update(msg);
-  std::string hash = sha1.final();
+  std::vector<uint8_t> out(SHA256_HASH_SIZE);
 
-  std::cout << hash << " " << (hash == "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12") << std::endl;
+  hmac_sha256(secret.data(), secret.size(), data.data(), data.size(), out.data(), out.size());
+
+  for (uint8_t x : out) {
+    res << std::hex << std::setfill('0') << std::setw(2) << (int)x;
+  }
+
+  std::cout << "Message : " << data << std::endl;
+  std::cout << "Secret  : " << secret << std::endl;
+  std::cout << "HMAC    : " << res.str() << std::endl;
+
+  assert(res.str() == "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8");
 
   return 0;
 }
